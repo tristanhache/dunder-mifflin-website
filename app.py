0840fe1e7149dbb5f2a1df880573cdc404aa59e0
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-03-03 18:04:30 +0000

    Update

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/ma-deuxieme-page")
+def page2():
+    return render_template("page-2.html")
